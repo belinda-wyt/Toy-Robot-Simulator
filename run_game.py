b56649aa24b19e64fad58e393f9833819efe9341
belinda-wyt <belinda.yuting.wang@live.com>
2021-08-02 21:32:31 +1000

    testcase finish
    1, Finish all testcases
    2, Finish the structure of application code

---
+++ run_game.py
@@ -0,0 +1,12 @@
+from cmd import proc_cmd
+from toy_robot import ToyRobot
+
+def run_game():
+    print("Game start... ")
+    robot = ToyRobot()
+    while True:
+        proc_cmd(robot)
+
+
+if __name__ == "__main__":
+    run_game()
---
+++ toy_robot.py
@@ -0,0 +1,22 @@
+"""
+The definition of the ToyRobot
+"""
+
+class ToyRobot:
+    def __init__(self):
+        pass
+
+    def place(self, x, y, facing):
+        pass
+
+    def move(self):
+        pass
+
+    def right(self):
+        pass
+
+    def left(self):
+        pass
+
+    def report(self):
+        pass
